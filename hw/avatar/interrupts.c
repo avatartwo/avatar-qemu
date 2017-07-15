caa141a160858dbf8cbab9b16ebf76598883b3aa
mariusmue <marius.muench@eurecom.fr>
2017-07-15 12:08:37 +0200

    starting on interrupt-injection

---
+++ hw/avatar/interrupts.c
@@ -0,0 +1,29 @@
+
+#include "qemu/osdep.h"
+#include "qemu-common.h"
+#include "qmp-commands.h"
+#include "qapi/error.h"
+
+#include "hw/sysbus.h"
+#include "sysemu/sysemu.h"
+
+#ifdef TARGET_ARM
+#include "target/arm/cpu.h"
+#elif TARGET_MIPS
+#include "target/mips/cpu.h"
+#endif
+
+void qmp_avatar_arm_irq(int64_t num_cpu, int64_t num_irq, Error **errp)
+{
+#ifdef TARGET_ARM
+    printf("In do_interrupt\n");
+    CPUState *cpu = CPU(qemu_get_cpu(num_cpu));
+    //cpu_interrupt(cpu, num_irq);
+    ARMCPU *armcpu = ARM_CPU(qemu_get_cpu(num_cpu));
+    //armcpu->pending |= 1<< irq
+    //CPUARMState *env = &armcpu->env;
+    armv7m_nvic_set_pending(armcpu, num_irq);
+#endif
+
+}
+
