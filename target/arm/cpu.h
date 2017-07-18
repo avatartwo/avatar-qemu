6557aef5f0d4afe4e306e17e8343a7aca8c5a8dc
mariusmue <marius.muench@eurecom.fr>
2017-07-18 19:29:51 +0200

    Banked registers!

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -1959,6 +1959,7 @@ enum arm_features {
     ARM_FEATURE_VBAR, /* has cp15 VBAR */
     ARM_FEATURE_M_SECURITY, /* M profile Security Extension */
     ARM_FEATURE_M_MAIN, /* M profile Main Extension */
+    ARM_FEATURE_CONFIGURABLE, /* created with avatar */
 };
 
 static inline int arm_feature(CPUARMState *env, int feature)
