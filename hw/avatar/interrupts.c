ebfb195f5a03aee427941de373eef18d0a1ca75e
mariusmue <marius.muench@eurecom.fr>
2018-01-22 15:01:30 +0100

    ignore/unignore irq returns

--- hw/avatar/interrupts.c
+++ hw/avatar/interrupts.c
@@ -29,6 +29,7 @@ extern  QemuAvatarMessageQueue *rmem_tx_queue_ref;
 static uint64_t req_id;
 
 static bool armv7m_exception_handling_enabled = false;
+static uint8_t ignore_irq_return_map[32] = {0};
 
 void avatar_armv7m_nvic_forward_write(uint32_t offset, uint32_t value, unsigned size){
     int ret;
@@ -93,10 +94,22 @@ void qmp_avatar_armv7m_disable_irq(Error **errp)
     qemu_log_flush();
 }
 
-void qmp_avatar_armv7m_inject_irq(int64_t num_cpu,
-                                  int64_t num_irq, Error **errp)
+
+void qmp_avatar_armv7m_ignore_irq_return(int64_t num_irq, Error **errp)
+{
+    ignore_irq_return_map[num_irq/8] |= 1 << num_irq % 8;
+}
+
+void qmp_avatar_armv7m_unignore_irq_return(int64_t num_irq, Error **errp)
+{
+    ignore_irq_return_map[num_irq/8] &= 0 << num_irq % 8;
+}
+
+void qmp_avatar_armv7m_inject_irq(int64_t num_cpu,int64_t num_irq, Error **errp)
 {
 #ifdef TARGET_ARM
+
+
     if(!armv7m_exception_handling_enabled){
         qemu_log_mask(LOG_AVATAR, "invalid armv7m interrupt injection attempt");
         qemu_log_flush();
@@ -115,15 +128,21 @@ void avatar_armv7m_exception_exit(int irq, uint32_t type)
     V7MInterruptResp resp;
     V7MInterruptReq request = {req_id++, irq, type};
 
-    qemu_log_mask(LOG_AVATAR, "Returning form 0x%x\n", irq);
-    memset(&resp, 0, sizeof(resp));
+    if( ignore_irq_return_map[irq/8] & 1 << irq % 8 )
+    {
+        qemu_log_mask(LOG_AVATAR, "Returning form 0x%x - Ignored by avatar\n", irq);
+    }
+    else{
+        qemu_log_mask(LOG_AVATAR, "Returning form 0x%x\n", irq);
+        memset(&resp, 0, sizeof(resp));
 
-    qemu_avatar_mq_send(irq_tx_queue_ref, &request, sizeof(request));
-    ret = qemu_avatar_mq_receive(irq_rx_queue_ref, &resp, sizeof(resp));
+        qemu_avatar_mq_send(irq_tx_queue_ref, &request, sizeof(request));
+        ret = qemu_avatar_mq_receive(irq_rx_queue_ref, &resp, sizeof(resp));
 
-    if(!resp.success || (resp.id != request.id)){
-        error_report("ARMv7mInterruptRequest failed (%d)!\n", ret);
-        exit(1);
+        if(!resp.success || (resp.id != request.id)){
+            error_report("ARMv7mInterruptRequest failed (%d)!\n", ret);
+            exit(1);
+        }
     }
 }
 
