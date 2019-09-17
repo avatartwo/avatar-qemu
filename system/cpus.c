098117934ab148200010b9a2290519e14dc87d11
Marius Muench <marius.muench@eurecom.fr>
2019-09-17 15:53:59 +0200

    Pretender (#3)
    * ARM: added a configurable machine
    
    * Small fixes
    
    * Fixed compilation error
    
    * Support for system-wide named semaphores
    
    * Added support for message queues
    
    * IPC: fixes permission on message queues and semaphores creation
    
    * Message queues: non-blocking
    
    Conflicts:
    	include/qemu/thread-posix.h
    
    * Avatar: Modifications to the IPC interface
    
    Extracted from 6e31210a3c13f4b6db7d7db5262cc47278e1f693
    
    * Started the memory forwarder device
    
    * Configurable: entry address and endianness
    
    * somewhat working remotememoryreadrequests
    
    * RemoteMemory!
    
    * Experimental removal of ram-resizable flag
    
    * Small fixes on the configurable machine
    
    * error handling in remote memory
    
    * integrated change in qapi
    
    * Hotfix: Don't bail out on failed rmr/rmw
    
    * Fixed bug regarding entrypoint, removed load_kernel and set_endianness from configurable machine
    
    * Cleaned up configurable machine and added MIPS-support
    
    * Adjusted RMemory to hold pc
    
    * bugfix in configurable machine
    
    * Banked registers!
    
    * move header files to include/hw/avatar
    
    * Refactored avatar mqueueing
    
    * starting on interrupt-injection
    
    * Unclean hackish commit to be squashed later
    
    * WIP: interrupt_exec and avatar_log
    
    * added log_item for avatar
    
    * minimal changes
    
    * nvic-related changes (untested!)
    
    * bugfixes
    
    * Banked registers!
    
    * nvic-write-forward update
    
    * let\'s return xpsr and not cpsr
    
    * Refactored avatar mqueueing
    
    * new logging for interrupts
    
    * move header files to include/hw/avatar
    
    * minor fixes
    
    * ignore/unignore irq returns
    
    * not enabling - stuff should be fine
    
    * additional interrupt notifications and acks
    
    * bugfix
    
    * Allow board init for more than armv7m
    
    * format strings are hard
    
    * Add set_nvic_base to qmp
    
    * I meant vector table base, not nvic base
    
    * fix compile on 18.04
    
    * Update to comply with qemu-3.1
    
    * cpu-model -> cpu-type
    
    * make usage of the new ARM_CPU_TYPE_NAME macro
    
    * temporary race condition hotfix: don't emit qapi_resume/stop

--- system/cpus.c
+++ system/cpus.c
@@ -709,7 +709,7 @@ int vm_stop(RunState state)
         return 0;
     }
 
-    return do_vm_stop(state, true);
+    return do_vm_stop(state, false);
 }
 
 /**
@@ -734,8 +734,8 @@ int vm_prepare_start(bool step_pending)
      * the STOP event.
      */
     if (runstate_is_running()) {
-        qapi_event_send_stop();
-        qapi_event_send_resume();
+        //qapi_event_send_stop();
+        //qapi_event_send_resume();
         return -1;
     }
 
@@ -748,7 +748,7 @@ int vm_prepare_start(bool step_pending)
     }
 
     /* We are sending this now, but the CPUs will be resumed shortly later */
-    qapi_event_send_resume();
+    //qapi_event_send_resume();
 
     cpu_enable_ticks();
     runstate_set(state);
--- util/log.c
+++ util/log.c
@@ -503,6 +503,8 @@ const QEMULogItem qemu_log_items[] = {
       "open a separate log file per thread; filename must contain '%d'" },
     { CPU_LOG_TB_VPU, "vpu",
       "include VPU registers in the 'cpu' logging" },
+    { LOG_AVATAR, "avatar",
+      "Show avatar related events\n" },
     { 0, NULL, NULL },
 };
 
