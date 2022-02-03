f8c255f6106be6f55f92dd88f185e112fa282930
sampl <paul.olivier@eurecom.fr>
2022-02-03 17:34:10 +0100

    New CONFIG_AVATAR option

--- target/arm/tcg/m_helper.c
+++ target/arm/tcg/m_helper.c
@@ -24,8 +24,9 @@
 #if !defined(CONFIG_USER_ONLY)
 #include "hw/intc/armv7m_nvic.h"
 #endif
-
+#ifdef CONFIG_AVATAR
 #include "hw/avatar/interrupts.h"
+#endif
 
 
 static void v7m_msr_xpsr(CPUARMState *env, uint32_t mask,
@@ -1404,7 +1405,9 @@ static void do_v7m_exception_exit(ARMCPU *cpu)
                   " previous exception %d\n",
                   excret, env->v7m.exception);
 
+#ifdef CONFIG_AVATAR
     avatar_armv7m_exception_exit(env->v7m.exception, excret);
+#endif
 
     if ((excret & R_V7M_EXCRET_RES1_MASK) != R_V7M_EXCRET_RES1_MASK) {
         qemu_log_mask(LOG_GUEST_ERROR, "M profile: zero high bits in exception "
