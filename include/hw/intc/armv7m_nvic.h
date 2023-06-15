ab59740f7b5b032cfec2d7479045eecaf6bdc2f3
Florian Albrecht <florian.albrecht.public@outlook.de>
2023-06-15 15:34:59 +0200

    Cleanup of interrupt hack

--- include/hw/intc/armv7m_nvic.h
+++ include/hw/intc/armv7m_nvic.h
@@ -206,10 +206,4 @@ static inline bool armv7m_nvic_can_take_pending_exception(NVICState *s)
 }
 #endif
 
-/* Avatar2 addition to enable interrupt injection
- */
-#ifdef CONFIG_AVATAR
-bool armv7m_nvic_enable_all_irqs(void *opaque);
-#endif
-
 #endif
