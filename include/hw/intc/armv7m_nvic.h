612f55242e77fb89ef44d0a1beafeb5034c0c6f7
rawsample <paul.olivier@eurecom.fr>
2024-11-06 18:24:21 +0100

    remove poisoned GCC

--- include/hw/intc/armv7m_nvic.h
+++ include/hw/intc/armv7m_nvic.h
@@ -208,9 +208,7 @@ static inline bool armv7m_nvic_can_take_pending_exception(NVICState *s)
 
 /* Avatar2 Addition to enable interrupt injection
  */
-#ifdef CONFIG_AVATAR
 bool armv7m_nvic_enable_all_irqs(void *opaque);
-#endif
 
 
 #endif
